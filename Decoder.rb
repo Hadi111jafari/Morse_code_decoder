4db162714c1d744e03fbb6d09901cab55a77b5b0
zkr024 <zkr.bm.02403@gmail.com>
2022-06-16 09:49:30 -0500

    Correct Linters erros

--- Decoder.rb
+++ Decoder.rb
@@ -1,49 +1,48 @@
-$morse_dict = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z',
-    ' ' => ' '
-  }
-  
-  def decode_char(char)
-    $morse_dict[char]
-  end
-  puts decode_char '.-'
-  
-  def decode_word(word)
-    decoded_morse = ''
-    word.split(/ /).each { |letter| decoded_morse += decode_char(letter) }
-    return decoded_morse
-  end
-  puts decode_word '-- -.--'
-  
-  def decode(sentence)
-    decoded_morse = ''
-    sentence.split(/   /).each { |word| decoded_morse += "#{decode_word(word)} " }
-    return decoded_morse
-  end
-  puts decode '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
-  
+@morse_dict = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  ' ' => ' '
+}
+
+def decode_char(char)
+  @morse_dict[char]
+end
+puts decode_char '.-'
+
+def decode_word(word)
+  decoded_morse = ''
+  word.split(/ /).each { |letter| decoded_morse += decode_char(letter) }
+  decoded_morse
+end
+puts decode_word '-- -.--'
+
+def decode(sentence)
+  decoded_morse = ''
+  sentence.split(/   /).each { |word| decoded_morse += "#{decode_word(word)} " }
+  decoded_morse
+end
+puts decode '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
